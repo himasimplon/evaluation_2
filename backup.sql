1d43a29ce8ccecd43805c789a374b2143c7fd940
ilam <hima14000@gmail.com>
2018-05-28 12:49:39 +0200

    ajout

---
+++ backup.sql
@@ -0,0 +1,46 @@
+
+    
+
+
+       
+
+       INSERT INTO bathroom 
+VALUES (1, 'shower'),
+		(2, 'bath'),
+		(3, 'shower and bath');	    
+
+
+INSERT INTO bed
+VALUES (1, 'single'),
+		(2, 'double queen size'),
+		(3, 'double king size');
+
+
+INSERT INTO category_service
+VALUES (1, 'equipment'),
+		(2, 'well being'),
+		(3, 'meal'),
+		(4, 'special');	
+
+
+INSERT INTO customer (name, first_name,phone ,mail password)
+VALUES ('Daniel', 'Plaza','0680808080','daniel.plaza@gmail.com', '14 avenue henry dunant caen',  'mzt20'),
+('malou', 'fabrice','07O7O7O707', 'malou.m@outlook.fr','22 rue des Morito  anger','ZER90' ),
+('dupont', 'kathy','0660606060','kathykatyy@orange.fr' '13 avenue des Champs Rouge  paris','KKK15'),
+('Jean', 'Joelle','0556285200','jojoko@free.fr', 'Abis rue picasso  toulous',  'JJJ04' );
+
+
+
+
+INSERT INTO state
+VALUES (1, 'in progress'),
+		(2, 'paid'),
+		(3, 'canceled');
+
+
+
+
+
+
+
+
